045020835f73c1340db526c0a3c4561ede389c10
ntvicse <32760540+ntvicse@users.noreply.github.com>
2018-03-17 00:55:35 +0700

    ex2
    ex2

--- Ex2/ws.php
+++ Ex2/ws.php
@@ -26,9 +26,21 @@
   //       parent::__construct($request);
   //     }
   //     public function call() {
+        // function deg2rad($deg) {
+        //    return ($deg * $pi / 180);
+        // }
+        function distanceEarth($lat1d, $lon1d, $lat2d, $lon2d){
+          $lat1r = deg2rad($lat1d);
+          $lon1r = deg2rad($lon1d);
+          $lat2r = deg2rad($lat2d);
+          $lon2r = deg2rad($lon2d);
+          $u = sin(($lat2r - $lat1r)/2);
+          $v = sin(($lon2r - $lon1r)/2);
+          return 2.0 * 6371 * asin(sqrt($u * $u + cos($lat1r) * cos($lat2r) * $v * $v));
+        }
         if (isset($_GET)) {
             if(isset($_GET['lat1']) && isset($_GET['lng1']) &&isset($_GET['lat2'])&&isset($_GET['lng2'])){
-              echo "Complete!";
+              echo distanceEarth($_GET['lat1'], $_GET['lng1'], $_GET['lat2'], $_GET['lng2']);
             }
         } else {
             return "Only accepts GET requests";
